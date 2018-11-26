239f1206f15411cd0f363ae3e4f628b63d5335cd
unknown <chrisduroseau@yahoo.com>
2018-11-25 22:28:34 -0500

    updated version

---
+++ app/data/friends.js
@@ -0,0 +1,66 @@
+var friendsArray = [
+    {
+      name: "Ahmed",
+      photo: "ahmed@example.com",
+      scores: [
+              3,2,4,2,1,5,5,3,1,2,
+              ],
+
+      name: "Joe",
+      photo: "ahmed@example.com",
+      scores: [
+              1,2,3,2,1,3,5,2,1,3
+              ],
+
+      name: "Mal",
+      photo: "ahmed@example.com",
+      scores: [
+              1,3,3,2,1,3,4,2,2,2
+              ],
+
+      name: "Rory",
+      photo: "ahmed@example.com",
+      scores: [
+              2,2,3,2,1,3,1,2,1,4
+              ],
+
+      name: "Kim",
+      photo: "ahmed@example.com",
+      scores: [
+              5,2,3,2,5,3,5,2,5,5
+              ],
+
+      name: "Lisa",
+      photo: "ahmed@example.com",
+      scores: [
+              4,2,1,2,1,4,1,3,1,2,
+              ],
+
+      name: "Keisha",
+      photo: "ahmed@example.com",
+      scores: [
+              5,5,5,5,5,5,5,5,5,5
+              ],
+
+      name: "Justin",
+      photo: "ahmed@example.com",
+      scores: [
+              1,1,1,1,1,1,1,1,1,1
+              ],
+
+      name: "Jill",
+      photo: "ahmed@example.com",
+      scores: [
+              2,2,2,2,2,2,2,2,2,2
+              ],
+
+      name: "Joe",
+      photo: "ahmed@example.com",
+      scores: [
+              5,4,5,4,5,4,5,4,5,4
+              ],
+    }
+  ];
+  
+  // Export the array
+  module.exports = friendsArray;
--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 const express = require ("express");
-const bodyParser = require("body-parser");
+const bodyParser = require ("body-parser");
+const path = require ("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text()),
+
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
